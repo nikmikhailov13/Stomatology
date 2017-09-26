0e676d0f509a5a33a37943bfa8a5a0e9da566f72
Nikita Mykhailov <nikmikhailov13@gmail.com>
2017-09-26 22:13:54 +0300

    Update DBWorker.java

--- src/main/java/sql/DBWorker.java
+++ src/main/java/sql/DBWorker.java
@@ -8,10 +8,9 @@ import org.postgresql.Driver;
 
 public class DBWorker {
 
-    private final String URL = "jdbc:postgresql://ec2-54-195-248-0.eu-west-1.compute.amazonaws.com:" +
-            "5432/d4vj8dpuiehhuf?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
-    private final String USER = "udotunkdbthbwa";
-    private final String PASSWORD = "05ab1c7af43d2b1410b09d2d600e9e390e45a4d2aa9ca7add0304062520391f4";
+    private final String URL =  " ";
+    private final String USER = " ";
+    private final String PASSWORD = " ";
 
     private Connection connection;
 
